ffb12eade4e8c1e4cbda53e2fead0d0e6b5c2dad
Nayem5522 <sahidhasannayembd@gmail.com>
2026-05-08 22:55:24 +0600

    Update urls.py

--- bot/plugins/urls.py
+++ bot/plugins/urls.py
@@ -1,22 +1,30 @@
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot.utils import Utilities
 from bot.screenshotbot import ScreenShotBot
 from bot.config import Config
 import datetime
 
-@ScreenShotBot.on_message(filters.private & (filters.video | filters.document | filters.text))
-async def handle_media(c, m):
-    # ভিডিও বা ইউআরএল চেক করা
-    if m.media:
-        if not Utilities.is_valid_file(m): return
-    elif not Utilities.is_url(m.text):
+@ScreenShotBot.on_message(filters.private & (filters.video | filters.document | filters.text) & filters.incoming)
+async def _(c, m):
+    # এডিট করা মেসেজ ইগনোর করা
+    if m.edit_date:
         return
 
-    snt = await m.reply_text("Processing your request... Please wait! 😴", quote=True)
+    # ফাইল চেক
+    if m.media:
+        if not Utilities.is_valid_file(m):
+            return
+    else:
+        if not Utilities.is_url(m.text):
+            return
 
+    snt = await m.reply_text("Processing... Please wait!", quote=True)
+
+    # ফাইল লিঙ্ক জেনারেট করা
     file_link = Utilities.generate_stream_link(m) if m.media else m.text
+
     duration = await Utilities.get_duration(file_link)
-    
     if isinstance(duration, str):
         await snt.edit_text("😟 Sorry! I cannot open the file.")
         return
@@ -26,6 +34,6 @@ async def handle_media(c, m):
         btns.append([InlineKeyboardButton("Generate Sample Video!", callback_data="smpl")])
 
     await snt.edit_text(
-        text=f"Total duration: `{datetime.timedelta(seconds=duration)}`",
+        text=f"Total duration: `{datetime.timedelta(seconds=duration)}` (`{duration}s`)",
         reply_markup=InlineKeyboardMarkup(btns),
     )
