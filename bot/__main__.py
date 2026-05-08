504f009b7c21d37878d3e46245c7a1854621ebda
Nayem5522 <sahidhasannayembd@gmail.com>
2026-05-08 20:25:09 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,8 @@
+# bot/__main__.py আপডেট করুন
 import logging
-
 from .screenshotbot import ScreenShotBot
 from .config import Config
+from keep_alive import keep_alive # নতুন ইমপোর্ট
 
 if __name__ == "__main__":
 
@@ -9,5 +10,8 @@ if __name__ == "__main__":
     logging.getLogger("pyrogram").setLevel(
         logging.INFO if Config.DEBUG else logging.WARNING
     )
-
+    
+    # পোর্ট ওপেন রাখার জন্য সার্ভার চালু করুন
+    keep_alive()
+    
     ScreenShotBot().run()
